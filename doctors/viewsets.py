f11365018dc9b4a9915dfbb63bee57b84d372b16
Àngel Arreola <angelarreola.cheesecake@gmail.com>
2026-01-14 19:32:58 -0600

    ✨ feat: Add `is_on_vacation` field to Doctor model and API endpoints to manage doctor vacation status.

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -1,3 +1,6 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework import viewsets
 from .models import Doctor
 from .serializers import DoctorSerializer
@@ -6,3 +9,17 @@ from .serializers import DoctorSerializer
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+
+    @action(["POST"], detail=True, url_path="set-on-vacation")
+    def set_on_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = True
+        doctor.save()
+        return Response({"status": "Doctor is on vacations now."})
+
+    @action(["POST"], detail=True, url_path="set-off-vacation")
+    def set_off_vacation(self, request, pk):
+        doctor = self.get_object()
+        doctor.is_on_vacation = False
+        doctor.save()
+        return Response({"status": "Doctor is not on vacations now."})
