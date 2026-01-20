faa1eb22f40f8ff395c9b9632d3c2fff22a601f5
Àngel Arreola <angelarreola.cheesecake@gmail.com>
2026-01-20 17:46:01 -0600

    ✨ feat: add email validation and contact number check for DoctorSerializer

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework import serializers
 from .models import Doctor, Department, DoctorAvailability, MedicalNote
 
@@ -6,6 +7,20 @@ class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = "__all__"
+
+    def validate_email(self, value):
+       # Regex pattern: valid email format with @example.com domain
+        email_regex = r'^[a-zA-Z0-9._%+-]+@example\.com$'
+        if not re.match(email_regex, value):
+            return value
+        raise serializers.ValidationError("Email must be in the format: username@example.com")
+
+    def validate(self, attrs):
+        if len(attrs["contact_number"]) < 10 and attrs["is_on_vacation"] == True:
+            raise serializers.ValidationError(
+                "Please update the contact information with a valid phone number before setting the doctor on vacation."
+            )
+        return super().validate(attrs)
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
