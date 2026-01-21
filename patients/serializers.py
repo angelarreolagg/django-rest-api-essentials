1c14b343f26b517c81ae9c4c8e288bb1cc299979
Àngel Arreola <angelarreola.cheesecake@gmail.com>
2026-01-20 18:18:22 -0600

    ✨ feat: add age calculation to PatientSerializer

--- patients/serializers.py
+++ patients/serializers.py
@@ -1,3 +1,4 @@
+from datetime import date
 from rest_framework import serializers
 from .models import Patient, Insurance, MedicalRecord
 from bookings.serializers import AppointmentSerializer
@@ -5,12 +6,14 @@ from bookings.serializers import AppointmentSerializer
 
 class PatientSerializer(serializers.ModelSerializer):
     appointments = AppointmentSerializer(many=True, read_only=True)
+    age = serializers.SerializerMethodField()
     class Meta:
         model = Patient
         fields = [
             "id",
             "first_name",
             "last_name",
+            "age",
             "date_of_birth",
             "contact_number",
             "email",
@@ -18,6 +21,10 @@ class PatientSerializer(serializers.ModelSerializer):
             "medical_history",
             "appointments",
         ]
+    
+    def get_age(self, obj):
+        age_td = date.today() - obj.date_of_birth
+        return age_td.days // 365
 
 
 class InsuranceSerializer(serializers.ModelSerializer):
