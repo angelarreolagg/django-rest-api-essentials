ba54db1616be4fbcaad1779a47ee5908fc6a2b70
Àngel Arreola <angelarreola.cheesecake@gmail.com>
2026-01-21 18:51:09 -0600

    ✨ feat: add appointments action to DoctorViewSet for managing doctor appointments

--- doctors/viewsets.py
+++ doctors/viewsets.py
@@ -2,9 +2,12 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets
+from rest_framework import status
 from .models import Doctor
 from .permissions import IsDoctor
 from .serializers import DoctorSerializer
+from bookings.serializers import AppointmentSerializer
+from bookings.models import Appointment
 
 
 class DoctorViewSet(viewsets.ModelViewSet):
@@ -26,3 +29,21 @@ class DoctorViewSet(viewsets.ModelViewSet):
         doctor.is_on_vacation = False
         doctor.save()
         return Response({"status": "Doctor is not on vacations now."})
+    
+    @action(["GET", "POST"], detail=True, serializer_class=AppointmentSerializer)
+    def appointments(self, request, pk):
+        doctor = self.get_object()
+        
+        if request.method == 'POST':
+            data = request.data.copy()
+            data['doctor'] = doctor.id
+            serializer = AppointmentSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.method == 'GET':
+            appointments = Appointment.objects.filter(doctor=doctor)
+            serializer = AppointmentSerializer(appointments, many=True)
+            return Response(serializer.data)
+    
